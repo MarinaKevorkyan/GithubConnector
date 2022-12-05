ce29d53d50a4eacdf865acb4c0479ab5de0ebffd
MarinaKevorkyan <mkevorkyan@mulesoft.com>
2022-12-05 14:01:23 -0300

    probando key

--- src/main/java/com/mulesoft/connector/github/internal/Error/GithubConnectorErrorType.java
+++ src/main/java/com/mulesoft/connector/github/internal/Error/GithubConnectorErrorType.java
@@ -3,8 +3,6 @@ package com.mulesoft.connector.github.internal.Error;
 import org.mule.runtime.extension.api.error.ErrorTypeDefinition;
 import org.mule.runtime.extension.api.error.MuleErrors;
 
-import java.util.Arrays;
-
 public enum GithubConnectorErrorType implements ErrorTypeDefinition<GithubConnectorErrorType> {
     INVALID_CONNECTION(MuleErrors.CONNECTIVITY),
     CONNECTIVITY(MuleErrors.CONNECTIVITY),
